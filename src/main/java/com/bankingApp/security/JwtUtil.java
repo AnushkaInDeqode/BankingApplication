926ead317ad2eb2826927ff10cabe6b491b1fb6c
AnushkaInDeqode <akesarwani@deqode.com>
2023-05-24 17:35:43 +0530

    Initial commit

--- src/main/java/com/bankingApp/security/JwtUtil.java
+++ src/main/java/com/bankingApp/security/JwtUtil.java
@@ -2,18 +2,16 @@ package com.bankingApp.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class JwtUtil {
-    private Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private String secretKey = "mySecretKey";
     public static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60;
 
     public String generateToken(UserDetails userDetails) {
@@ -23,12 +21,12 @@ public class JwtUtil {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
-                .signWith(secretKey)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
     public String extractUsername(String token) {
-        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token, UserDetails userDetails) {
@@ -37,7 +35,8 @@ public class JwtUtil {
     }
 
     private boolean isTokenExpired(String token) {
-        final Date expiration = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getExpiration();
+        final Date expiration = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getExpiration();
         return expiration.before(new Date());
     }
 }
+
